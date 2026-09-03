f6363885d85a247ad3731c2b324d171a899d8053
Bhanusnehitha <bhanusnehitha13@gmail.com>
2026-09-03 12:01:21 +0530

    Time: 0 ms (100%), Space: 19.4 MB (55.14%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -0,0 +1,35 @@
+class Solution:
+    def wordBreak(self, s, wordDict):
+
+        wordDict = set(wordDict)
+        memo = {}
+
+        def dfs(start):
+
+            if start == len(s):
+                return [""]
+
+            if start in memo:
+                return memo[start]
+
+            result = []
+
+            for end in range(start + 1, len(s) + 1):
+
+                word = s[start:end]
+
+                if word in wordDict:
+
+                    remaining = dfs(end)
+
+                    for sentence in remaining:
+
+                        if sentence:
+                            result.append(word + " " + sentence)
+                        else:
+                            result.append(word)
+
+            memo[start] = result
+            return result
+
+        return dfs(0)
